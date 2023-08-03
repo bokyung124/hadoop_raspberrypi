3ef885988c91ba37ce30eaec456d0f3062f0ca07
BoKyung <leebk1124@gmail.com>
2023-08-03 11:24:11 +0900

    sqoop.sh

--- sqoop.sh
+++ sqoop.sh
@@ -1,8 +1,11 @@
-# temperature table에 id 컬럼 추가
-alter table temperature add column id int(20) PRIMARY KEY AUTO_INCREMENT;
-
 # sqoop 설치
 sudo wget https://archive.apache.org/dist/sqoop/1.4.6/sqoop-1.4.6.bin__hadoop-2.0.4-alpha.tar.gz
 
 # HDFS 연결
 sudo bin/sqoop import --connect jdbc:mysql://localhost:3306/sensor --table temperature --username root --password dlqhrud-1124 --target-dir /user/pi/temperature
+
+# 파일 확인
+hadoop fs -ls /user/pi/temperature
+
+hadoop fs -cat /user/pi/temperature/part-m-00000
+...
