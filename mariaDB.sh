cbc5e31d5e996897d3a2677f36772c6ae7c318f4
BoKyung <leebk1124@gmail.com>
2023-08-03 11:22:35 +0900

    mariaDB.sh

--- mariaDB.sh
+++ mariaDB.sh
@@ -1,3 +1,17 @@
+# install
+sudo apt-get install mariadb-client mariadb-server
+
+# port 확인
+show global variables like 'port';
+    # 3306
+# raspberrypi에서 allow
+sudo ufw allow 3306
+
+# 유저 권한 부여
+grant all privileges on *.* to 'root'@'localhost' identified by 'dlqhrud-1124';
+
+flush privileges;
+
 # sensor database 생성
 create database sensor;
 
@@ -10,3 +24,5 @@ create table temperature (
     hum     float
 );
 
+# sqoop 이용하기 위해 id 컬럼 추가
+alter table temperature add column id int(20) PRIMARY KEY AUTO_INCREMENT;
